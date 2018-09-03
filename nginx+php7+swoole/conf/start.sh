5b3492e7e63635af667f6a0d182cfb5e77814bd8
Sure Yu <yushuai@yeelight.com>
2018-09-03 19:11:43 +0800

    start.sh

--- nginx+php7+swoole/conf/start.sh
+++ nginx+php7+swoole/conf/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-
+# 用 swoole 运行 laravel #
+# Document： https://github.com/hhxsv5/laravel-s/blob/master/README-CN.md#%E8%BF%90%E8%A1%8C #
+cd /data/www && /usr/local/php/bin/php artisan laravels start
 /usr/local/nginx/sbin/nginx
-cd /data/www && /usr/local/php/bin/php artisan laravels start -d
